79e913265d42b24a79725a9b664e494e6e31e39b
zerowx6688 <102398521+zerowx6688@users.noreply.github.com>
2023-10-17 20:40:37 +0800

    Update box.sh
    美化

--- box.sh
+++ box.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 定义颜色样式
+Green="\e[32m"
+Red="\e[31m"
+Default="\e[0m"
+
 # 定义SWAP安装/卸载函数
 function swap() {
     wget -O "/root/swap.sh" "https://raw.githubusercontent.com/zerowx6688/box/main/swap.sh" --no-check-certificate -T 30 -t 5 -d
@@ -12,38 +17,40 @@ function swap() {
 
 # 定义更新脚本函数
 function update() {
-    echo "正在更新脚本..."
+    echo -e "${Green}正在更新脚本...${Default}"
     wget -O "$0" "https://raw.githubusercontent.com/zerowx6688/box/main/box.sh" --no-check-certificate -T 30 -t 5 -d
-    echo "更新完成"
+    echo -e "${Green}更新完成${Default}"
 }
 
 # 主菜单函数
 function start_menu() {
     while true; do
         clear
-        echo "请选择操作:"
-        echo "1. 更新脚本"
-        echo "2. 执行SWAP一键安装/卸载脚本"
-        echo "999. 退出"
+        echo -e "${Green}==================================================${Default}"
+        echo -e " ${Green}1. 更新脚本${Default}"
+        echo -e " ${Green}2. 执行SWAP一键安装/卸载脚本${Default}"
+        echo -e " ${Green}3. 退出脚本${Default}"
+        echo -e "${Green}==================================================${Default}"
+        echo -e "请选择操作:"
 
         read choice
         case $choice in
             1)
                 update
-                echo "按 Enter 键返回主菜单"
+                echo -e "按 Enter 键返回主菜单"
                 read -p ""
                 ;;
             2)
                 swap
-                echo "按 Enter 键返回主菜单"
+                echo -e "按 Enter 键返回主菜单"
                 read -p ""
                 ;;
-            999)
+            3)
                 echo "退出脚本"
                 exit 0
                 ;;
             *)
-                echo "无效的选项"
+                echo -e "无效的选项"
                 ;;
         esac
     done
