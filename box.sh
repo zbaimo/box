91d7ac9a0159b2393a6034966ec87462a54b0b81
zerowx6688 <102398521+zerowx6688@users.noreply.github.com>
2023-10-17 20:29:01 +0800

    Update box.sh
    更新脚本

--- box.sh
+++ box.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义SWAP安装/卸载函数
-function swapsh() {
+function swap() {
     wget -O "/root/swap.sh" "https://raw.githubusercontent.com/zerowx6688/box/main/swap.sh" --no-check-certificate -T 30 -t 5 -d
     chmod +x "/root/swap.sh"
     chmod 777 "/root/swap.sh"
@@ -9,19 +9,31 @@ function swapsh() {
     echo "你也可以输入 bash /root/swap.sh 来手动运行"
     bash "/root/swap.sh"
 }
+# 定义更新脚本函数
+function update() {
+    echo "正在更新脚本..."
+    wget -O "$0" "https://raw.githubusercontent.com/zerowx6688/box/main/box.sh" --no-check-certificate -T 30 -t 5 -d
+    echo "更新完成"
+}
 
 # 主菜单函数
 function start_menu() {
     while true; do
         clear
         echo "请选择操作:"
+        echo "0. 更新脚本"
         echo "1. 执行SWAP一键安装/卸载脚本"
         echo "999. 退出"
 
         read choice
         case $choice in
+            0)
+                updata
+                echo "按 Enter 键返回主菜单"
+                read -p ""
+                ;;
             1)
-                swapsh
+                swap
                 echo "按 Enter 键返回主菜单"
                 read -p ""
                 ;;
