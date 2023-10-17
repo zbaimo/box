6c362573dcc4e293e402d4f9eb3bf88b9b4b2075
zerowx6688 <102398521+zerowx6688@users.noreply.github.com>
2023-10-17 20:21:34 +0800

    Update Box.sh

--- Box.sh
+++ Box.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义SWAP安装/卸载函数
-function swapsh(){
+function swapsh() {
     wget -O "/root/swap.sh" "https://raw.githubusercontent.com/zerowx6688/box/main/swap.sh" --no-check-certificate -T 30 -t 5 -d
     chmod +x "/root/swap.sh"
     chmod 777 "/root/swap.sh"
@@ -11,19 +11,21 @@ function swapsh(){
 }
 
 # 主菜单函数
-function start_menu(){
+function start_menu() {
     while true; do
         clear
         echo "请选择操作:"
         echo "1. 执行SWAP一键安装/卸载脚本"
-        echo "2. 退出"
+        echo "999. 退出"
 
         read choice
         case $choice in
             1)
                 swapsh
+                echo "按 Enter 键返回主菜单"
+                read -p ""
                 ;;
-            2)
+            999)
                 echo "退出脚本"
                 exit 0
                 ;;
