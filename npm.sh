469156a6113d7fd24839b317f52822374ba65c77
zerowx6688 <102398521+zerowx6688@users.noreply.github.com>
2023-10-19 10:11:50 +0800

    Update npm.sh

--- npm.sh
+++ npm.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# 设置绿色文本颜色
+GREEN='\033[0;32m'
+NC='\033[0m' # 重置颜色
+
+# 函数用于显示进度条
+show_progress() {
+    local duration="$1"
+    local progress=0
+    local block="■"
+    local progress_text=""
+
+    while [ $progress -lt 100 ]; do
+        progress=$((progress + 1))
+        sleep "$duration"
+        block_count=$((progress / 2))
+        progress_text="[$(printf "%0.s$block" $(seq 1 $block_count))$(printf "%0.s " $(seq $((block_count + 1)) 50))]"
+        echo -ne "\r${GREEN}进度: $progress_text $progress%${NC}"
+    done
+
+    echo -e "\n"
+}
+
+
 # 进入root目录
 cd /root
 
@@ -37,8 +60,8 @@ services:
       DB_MYSQL_PASSWORD: "npm"
       DB_MYSQL_NAME: "npm"
     volumes:
-      - ../../data:/data
-      - ../../letsencrypt:/etc/letsencrypt
+      - ./data:/data
+      - ./letsencrypt:/etc/letsencrypt
     depends_on:
       - db
 
@@ -51,8 +74,30 @@ services:
       MYSQL_USER: 'npm'
       MYSQL_PASSWORD: 'npm'
     volumes:
-      - ../../mysql:/var/lib/mysql
+      - ./mysql:/var/lib/mysql
 EOL
 
-# 执行docker-compose up -d
-docker compose up -d
+# 定义要使用的 Docker 镜像名
+IMAGE_NAME='jc21/nginx-proxy-manager:latest'
+
+# 检查是否有特定镜像名的 Docker 镜像
+if docker images --format '{{.Repository}}:{{.Tag}}' | grep -q "$IMAGE_NAME"; then
+    # 如果有特定镜像名的镜像，不显示下载消息
+    echo -e "${GREEN}本地已存在 $IMAGE_NAME 镜像，将直接启动容器.${NC}"
+else
+    # 如果没有特定镜像名的镜像，显示下载消息
+    echo -e "${GREEN}正在拉取 $IMAGE_NAME 镜像，可能需要一些时间...${NC}"
+fi
+
+# 执行 docker-compose up -d，并隐藏输出
+docker compose up -d > /dev/null 2>&1
+
+# 显示进度条
+show_progress 0.1
+
+# 输出安装完成消息
+echo -e "${GREEN}NPM 安装已完成${NC}"
+
+# 显示已开通的端口
+echo -e "${GREEN}已开通的端口：80, 443, 6600${NC}"
+
